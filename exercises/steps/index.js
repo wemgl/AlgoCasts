aae295f6dfd3fc7dce89ec58adbb1fb09ab15381
Wembley Leach <wembley@hey.com>
2022-04-13 20:24:21 -0400

    Implement steps question

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -2,7 +2,7 @@
 // Write a function that accepts a positive number N.
 // The function should console log a step shape
 // with N levels using the # character.  Make sure the
-// step has spaces on the right hand side!
+// step has spaces on the right-hand side!
 // --- Examples
 //   steps(2)
 //       '# '
@@ -17,6 +17,15 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n) {
+    let template = [];
+    for (let i = 0; i < n; i++) {
+        template.push(' ');
+    }
+    for (let i in template) {
+        template[i] = '#';
+        console.log(template.join(''));
+    }
+}
 
 module.exports = steps;
