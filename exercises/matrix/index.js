d46a928fb76eb10e566438da293ca8e6321320a1
Wembley Leach <wembley@hey.com>
2022-04-16 10:41:26 -0400

    Implement spiral matrix question

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,69 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+    let result = [];
+    let gridSize = n;
+
+    // Create NxN grid - O(n^2)
+    for (let r = 0; r < gridSize; r++) {
+        let columns = [];
+        for (let c = 0; c < gridSize; c++) {
+            columns.push(0);
+        }
+        result.push(columns);
+    }
+
+    let counter = 1;
+    let maxCount = n * n;
+    let r = 0, c = 0;
+
+    while (counter <= maxCount) {
+        // Increment the counter up the top row
+        while (c < gridSize && counter <= maxCount) {
+            result[r][c] = counter;
+            counter += 1;
+            c += 1;
+        }
+
+        // Increment the counter down the right column
+        r += 1;
+        c -= 1; // Correct c for last iteration of the previous while loop
+        while (r < gridSize && counter <= maxCount) {
+            result[r][c] = counter;
+            counter += 1;
+            r += 1;
+        }
+
+        // Increment the counter down the bottom row
+        c -= 1;
+        r -= 1; // Correct r for last iteration of the previous while loop
+        while (c >= 0 && counter <= maxCount) {
+            result[r][c] = counter;
+            counter +=1;
+            c -= 1;
+        }
+
+        // Increment the counter up the left column
+        r -= 1;
+        c += 1; // Correct c for last iteration of the previous loop
+        while (r >= 1 && counter <= maxCount) {
+            result[r][c] = counter;
+            counter += 1;
+            r -= 1;
+        }
+
+        // Shrink the grid
+        gridSize -= 1;
+
+        // Start at first row in the inner grid
+        r += 1;
+
+        // The inner square starts along the diagonal, e.g. (r0, c0) -> (r1, c1), (r2, c2), …, (rn, cn)
+        c = r;
+    }
+
+    return result;
+}
 
 module.exports = matrix;
