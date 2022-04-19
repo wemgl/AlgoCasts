cc56b7c43561ab57dbd4310777d742aba640bd80
Wembley Leach <wembley@hey.com>
2022-04-18 20:29:35 -0400

    Solve the Fibonacci problem

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -1,13 +1,27 @@
 // --- Directions
 // Print out the n-th entry in the fibonacci series.
 // The fibonacci series is an ordering of numbers where
-// each number is the sum of the preceeding two.
+// each number is the sum of the preceding two.
 // For example, the sequence
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
 //   fib(4) === 3
 
-function fib(n) {}
+function fib(n) {
+    let result = [0, 1];
+    if (n === 0) {
+        return result[0];
+    } else if (n === 1) {
+        return result[1];
+    } else {
+        for (let i = 2; i <= n; i++) {
+            let tmp = result[1];
+            result[1] = result[0] + result[1];
+            result[0] = tmp;
+        }
+        return result[1];
+    }
+}
 
 module.exports = fib;
