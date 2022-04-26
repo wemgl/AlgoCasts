7c9bca967873decf9039dafb07805aecc68f5158
Wembley Leach <wembley@hey.com>
2022-04-25 20:30:39 -0400

    Implement solution for n nodes from last LinkedList interview question

--- exercises/fromlast/index.js
+++ exercises/fromlast/index.js
@@ -11,6 +11,29 @@
 //    list.insertLast('d');
 //    fromLast(list, 2).data // 'b'
 
-function fromLast(list, n) {}
+function fromLast(list, n) {
+    if (!list.head) {
+        return null;
+    }
+
+    let slow = list.getFirst();
+    let fast = list.getFirst();
+
+    // Move fast up by n nodes
+    let count = 0;
+    while (count < n) {
+        fast = fast.next;
+        count += 1;
+    }
+
+    // Now move slow and fast one node at a time
+    while (fast.next) {
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    // When fast reaches the tail that means slow is n nodes from the tail, so return it
+    return slow;
+}
 
 module.exports = fromLast;
