bfc1d7801e36f9dd0f40170f2324644ee603fde1
Wembley Leach <wembley@hey.com>
2022-04-26 20:08:33 -0400

    Implement question to calculate the width of each row of a tree

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,38 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+// This implementation is a modified version of the depth first traversal algorithm.
+// Keep that in mind!
+function levelWidth(root) {
+    // Initialize array of counts for each level. Each index in the counts array
+    // is one level in the tree.
+    let counts = [0];
+    // Initialize a buffer containing two elements
+    // 1. The root node as the first element
+    // 2. A value to flag that the last child of a node has been reached (a.k.a stopper)
+    let buf = [root, null];
+
+    // Iterate over the buffer of nodes while there's more than one element
+    // (If there's a single element left, then that means only the stopper element is left
+    // and all the node's children have been processed).
+    while (buf.length > 1) {
+        // 1. Take the first element out of the buffer
+        let node = buf.shift();
+        // 2. Check whether the element is an actual node or the stopper (which is null)
+        //  If the element is an actual value, then add its children to the buffer and
+        //  increment the count of children at its row.
+        if (node) {
+            buf.push(...node.children);
+            counts[counts.length - 1] += 1;
+        } else {
+            // Else, push a new stopper at the end of buffer to signify that a new row's children
+            // is about to be processed.
+            buf.push(null);
+            // Add a new element to start counting
+            counts.push(0);
+        }
+    }
+    return counts;
+}
 
 module.exports = levelWidth;
