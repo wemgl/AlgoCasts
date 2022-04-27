4dd979e2af241e812b8f73254ebd3bb6e0d49785
Wembley Leach <wembley@hey.com>
2022-04-27 19:40:45 -0400

    Implement node insertion and contains check method for BST

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -11,6 +11,40 @@
 // class.  Contains should accept a 'data' argument
 // and return the Node in the tree with the same value.
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    insert(data) {
+        if (data < this.data) {
+            if (this.left) {
+                this.left.insert(data);
+            } else {
+                this.left = new Node(data);
+            }
+        } else if (data > this.data) {
+            if (this.right) {
+                this.right.insert(data);
+            } else {
+                this.right = new Node(data);
+            }
+        }
+    }
+
+    contains(data) {
+        if (data === this.data) {
+            return this;
+        } else if (data < this.data) {
+            return this.left.contains(data);
+        } else if (data > this.data) {
+            return this.right.contains(data);
+        } else {
+            return null;
+        }
+    }
+}
 
 module.exports = Node;
