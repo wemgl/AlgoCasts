3d6d561a43019fd348b11c23aee86473317dd99a
Wembley Leach <wembley@hey.com>
2022-04-28 20:37:21 -0400

    Implement mini JavaScript event library

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -4,16 +4,36 @@
 // have methods 'on', 'trigger', and 'off'.
 
 class Events {
-  // Register an event handler
-  on(eventName, callback) {}
+    constructor() {
+        this.registry = {};
+    }
 
-  // Trigger all callbacks associated
-  // with a given eventName
-  trigger(eventName) {}
+    // Register an event handler
+    on(eventName, callback) {
+        if (this.registry[eventName]) {
+            this.registry[eventName].push(callback);
+        } else {
+            this.registry[eventName] = [callback];
+        }
+    }
 
-  // Remove all event handlers associated
-  // with the given eventName
-  off(eventName) {}
+    // Trigger all callbacks associated
+    // with a given eventName
+    trigger(eventName) {
+        const callbacks = this.registry[eventName];
+        if (!callbacks) {
+            return;
+        }
+        for (let callback of callbacks) {
+            callback();
+        }
+    }
+
+    // Remove all event handlers associated
+    // with the given eventName
+    off(eventName) {
+        delete this.registry[eventName];
+    }
 }
 
 module.exports = Events;
