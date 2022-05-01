65c0cdcabc1a8c1f425698402661b9c1bb6c7343
Wembley Leach <wembley@hey.com>
2022-05-01 09:05:07 -0400

    Implement bubble sort algorithm

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,7 +2,20 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+    return arr;
+}
 
+function swap(arr, first, second) {
+    const tmp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = tmp;
 }
 
 function selectionSort(arr) {
