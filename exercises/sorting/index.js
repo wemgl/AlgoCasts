14d202a2269997a1bde4a6d09c3162e9718bf585
Wembley Leach <wembley@hey.com>
2022-05-01 09:28:20 -0400

    Add more comments to the bubble and selection sort algorithms

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,6 +2,8 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
+    // The largest number will be at the end of the array at the end of each iteration
+    // of this outer loop.
     for (let i = 0; i < arr.length; i++) {
         // This loop should iterate up to one less than the outer loop because it needs
         // and element to the right for its comparison (therefore it won't try to access
@@ -16,15 +18,23 @@ function bubbleSort(arr) {
 }
 
 function swap(arr, first, second) {
+    // Don't swap anything if the first and second indices are the same.
+    if (first === second) {
+        return;
+    }
     const tmp = arr[first];
     arr[first] = arr[second];
     arr[second] = tmp;
 }
 
 function selectionSort(arr) {
+    // The smallest number will be at the beginning of the array at the end of each iteration
+    // of this outer loop.
     for (let i = 0; i < arr.length; i++) {
         let indexOfMin = i;
         for (let j = i + 1; j < arr.length; j++) {
+            // The element we thought was the minimum is not actually the minimum
+            // (because the element at j is smaller), so update the indexOfMin.
             if (arr[indexOfMin] > arr[j]) {
                 indexOfMin = j;
             }
